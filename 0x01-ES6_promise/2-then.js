529537f79e9878b3ab07853e3241aaf03179ecbc
edddddr <endt6342@gmail.com>
2023-09-26 11:05:28 -0700

    Handling Prommise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,8 @@
-function handleResponseFromAPI(promise) {
-  promise
-    .then((response) => {
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => (
+    { status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch((error) => {
-      console.log('Got a response from the API');
-      return new Error('');
     });
 }
