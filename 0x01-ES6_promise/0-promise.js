cc51268d43e0ace6822b18855b6e0a09d6a263bb
edddddr <endt6342@gmail.com>
2023-09-26 06:27:29 -0700

    all node js file where setuped

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+import getResponseFromAPI from "./0-promise.js";
+
+const getResponseFromAPI = () => {
+	  return new Promise((resolve, reject) => {
+		      // Your asynchronous code here
+		  //     // Call resolve(value) when the asynchronous operation is successful
+		  //         // Call reject(error) when the asynchronous operation encounters an error
+		  //           });
+		  //           };
+		  //
+		  //           const response = getResponseFromAPI();
+		  //           console.log(response instanceof Promise); // true
