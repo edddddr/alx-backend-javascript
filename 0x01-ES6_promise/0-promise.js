b7bf3a4683bdbefd954944814e464b6365fcc2e2
edddddr <endt6342@gmail.com>
2023-09-26 06:35:09 -0700

    updated

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,3 @@
-import getResponseFromAPI from "./0-promise.js";
-
-const getResponseFromAPI = () => {
-	  return new Promise((resolve, reject) => {
-		      // Your asynchronous code here
-		  //     // Call resolve(value) when the asynchronous operation is successful
-		  //         // Call reject(error) when the asynchronous operation encounters an error
-		  //           });
-		  //           };
-		  //
-		  //           const response = getResponseFromAPI();
-		  //           console.log(response instanceof Promise); // true
+export default function getResponseFromAPI() {
+	  return new Promise((resolve, reject) => [resolve, reject]);
+}
