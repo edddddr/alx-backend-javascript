f3224cdfca93d5ace4a87ac143e4bfccebc5e6a9
edddddr <endt6342@gmail.com>
2023-09-26 11:15:16 -0700

    alltiem

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,28 @@
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([body, firstName, lastName]) => {
-      console.log('Got a response from the API');
-      console.log(`body: ${body}`);
-      console.log(`firstName: ${firstName}`);
-      console.log(`lastName: ${lastName}`);
+//function handleProfileSignup() {
+  //Promise.all([uploadPhoto(), createUser()])
+   // .then(([body, firstName, lastName]) => {
+     // console.log('Got a response from the API');
+     // console.log(`body: ${body}`);
+     // console.log(`firstName: ${firstName}`);
+     // console.log(`lastName: ${lastName}`);
+   // })
+   // .catch(() => {
+    //  console.log('Signup system offline');
+  //  });
+//}
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return uploadPhoto()
+    .then((data) => {
+      const { body } = data;
+      createUser()
+        .then((data) => {
+          const { firstName } = data;
+          const { lastName } = data;
+          console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log('Signup system offline'));
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
