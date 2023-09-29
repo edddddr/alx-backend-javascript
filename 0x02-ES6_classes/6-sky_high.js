fd609ab2ca423fd606181def77311cf94c3b38b9
edddddr <endt6342@gmail.com>
2023-09-29 10:34:10 -0700

    Sky

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,16 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, numFloors) {
+    super(sqft);
+    this._floors = numFloors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
