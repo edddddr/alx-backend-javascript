a6d85d5e9d982102d042557f8057f02df094c402
edddddr <endt6342@gmail.com>
2023-09-29 10:53:21 -0700

    cah

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,44 +1,32 @@
-import { Currency } from './3-currency.js';
+import Currency from './3-currency';
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
-    this._amount = typeof amount === 'number' ? amount : 0;
-    this._currency = currency instanceof Currency ? currency : null;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    if (typeof value === 'number') {
-      this._amount = value;
-    }
-  }
-
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    if (value instanceof Currency) {
-      this._currency = value;
-    }
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) this._currency = newCurrency;
   }
 
   displayFullPrice() {
-    if (this._currency) {
-      return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    } else {
-      return '';
-    }
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount === 'number' && typeof conversionRate === 'number') {
-      return amount * conversionRate;
-    } else {
-      return NaN;
-    }
+    return amount * conversionRate;
   }
 }
