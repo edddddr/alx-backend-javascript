c38d0bb9e1140a51035f683961031791455af41c
edddddr <endt6342@gmail.com>
2023-09-29 10:22:08 -0700

    hbtn

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = typeof name === 'string' ? name : '';
+    this._length = typeof length === 'number' ? length : 0;
+    this._students = Array.isArray(students) ? students : [];
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (Array.isArray(value)) {
+      this._students = value;
+    }
+  }
+}
