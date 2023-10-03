f9b298b35678b6afea2acc4d5688929709255a7c
edddddr <endt6342@gmail.com>
2023-10-03 04:47:27 +0000

    list of stu

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+/*Retrive a list of students in the arrat*/
+export default function getListStudents() {
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
